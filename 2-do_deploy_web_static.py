052178ef847d6babb86bbd992db1ebfde9dbb51e
Svcg17 <sofiavictoria17@gmail.com>
2019-08-20 06:58:10 +0000

    Fixed servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-The 2-do_deploy_web_static.py module
+The 2-do_deploy_web_static.pyy module
 Fabric script (based on the file 1-pack_web_static.py) that
 distributes an archive to your web servers, using the function do_deploy
 """
