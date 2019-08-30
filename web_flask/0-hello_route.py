7e423a1a9b14572625cf3e2fd85f3c883136a5e7
Svcg17 <sofiavictoria17@gmail.com>
2019-08-30 00:27:32 +0000

    Initial commit

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+The 0-hello_route module
+"""
+from flask import Flask, escape, request
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Starts a Flask web application
+        Return: a string
+    """
+    return 'Hello HBNB!'
+
+
+app.run(host='0.0.0.0', port=5000)
