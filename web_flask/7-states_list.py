a53c1b59d105956a50c42271d3e15b42271b59ec
Svcg17 <sofiavictoria17@gmail.com>
2019-08-31 20:37:18 +0000

    adding task 7 scripts

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+The 7-states_list module
+Starts a Flask web application
+"""
+from flask import Flask, escape, requesti, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext()
+def storage_app():
+    """ removes the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_hmtl():
+    """ Displays an HTTML page
+    """
+    s = storage.all('States')
+    return(s)
+
+
+app.run(host='0.0.0.0', port=5000)
