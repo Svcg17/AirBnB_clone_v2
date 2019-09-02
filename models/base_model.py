66a2d826ff93e912f3c57aa46b0f31f58e05e5ac
Svcg17 <sofiavictoria17@gmail.com>
2019-09-02 02:19:12 +0000

    Deleting condition in __str__ method

--- models/base_model.py
+++ models/base_model.py
@@ -37,6 +37,8 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+                if 'name' in kwargs.keys():
+                    self.name = kwargs['name']
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
@@ -46,10 +48,8 @@ class BaseModel:
         Return:
             returns a string of class name, id, and dictionary
         """
-        dict_cpy = self.__dict__.copy()
-        del dict_cpy['_sa_instance_state']
         return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, dict_cpy)
+            type(self).__name__, self.id, self.to_dict())
 
     def __repr__(self):
         """return a string representaion
