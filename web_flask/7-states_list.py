2950d42f515e70c0c4f5e88c6a4ceebed7467050
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-03 02:27:01 +0000

    Updating task 7 task

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,22 +5,23 @@ Starts a Flask web application
 """
 from flask import Flask, escape, request, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
-
-
-@app.teardown_appcontext()
-def storage_app(var=None):
-    """ removes the current SQLAlchemy Session
-    """
-    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def state_hmtl():
     """ Displays an HTTML page
     """
-    s = storage.all('States').values()
-    return render_template('7-states_list.html', states=states)
+    return render_template('7-states_list.html', state=storage.all('State').values())
+
+
+@app.teardown_appcontext
+def storage_app(var=None):
+    """ removes the current SQLAlchemy Session
+    """
+    storage.close()
+
 
 
 if __name__ == '__main__':
