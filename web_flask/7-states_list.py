c7ba6406163f8a783c2ca7dee12c0720309a8920
Sofia <sofiavictoria17@gmail.com>
2019-09-03 23:41:07 +0000

    passing states as an object

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,6 +5,7 @@ Starts a Flask web application
 """
 from flask import Flask, escape, request, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
@@ -13,7 +14,7 @@ def state_hmtl():
     """ Displays an HTTML page
     """
     return render_template('7-states_list.html',
-                           i=storage.all('State').values())
+                           i=storage.all(State).values())
 
 
 @app.teardown_appcontext
